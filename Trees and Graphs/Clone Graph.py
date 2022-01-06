807357f8d310a1b8187386b9d5e7d80b017642e2
robert njane <robert.ndungu@hotmail.com>
2022-01-06 10:05:29 +0300

    clone graph

--- Trees and Graphs/Clone Graph.py
+++ Trees and Graphs/Clone Graph.py
@@ -14,28 +14,7 @@ class Node:
 class Solution:
     #better
     """
-    Algorithm
-
-    We will use a hash map to store the reference of the copy of all the nodes that have already been visited and copied. 
-    The key for the hash map would be the node of the original graph and corresponding value would be the corresponding 
-    cloned node of the cloned graph.
-    
-    The visited is used to prevent cycles and get the cloned copy of a node.
-
-    Add the first node to the queue. Clone the first node and add it to visited hash map.
-
-    Do the BFS traversal.
-
-    Pop a node from the front of the queue.
-    Visit all the neighbors of this node.
-    If any of the neighbors was already visited then it must be present in the visited dictionary. Get the clone of this neighbor from visited in that case.
-    Otherwise, create a clone and store in the visited.
-    Add the clones of the neighbors to the corresponding list of the clone node.
-    """
-
-
-    
-    """ 
+    Algorithm:
     1. create a empty queue and a dict. create an empty return_list too.
     2. add the root node to the queue.
     while queue is not empty:
@@ -82,12 +61,33 @@ class Solution:
             return_list.append(current_new_node)
         return return_list[0]
 
-"""
-Complexity Analysis
+    """
+    Algorithm
 
+    We will use a hash map to store the reference of the copy of all the nodes that have already been visited and copied. 
+    The key for the hash map would be the node of the original graph and corresponding value would be the corresponding 
+    cloned node of the cloned graph.
+
+    The visited is used to prevent cycles and get the cloned copy of a node.
+
+    Add the first node to the queue. Clone the first node and add it to visited hash map.
+
+    Do the BFS traversal.
+
+    Pop a node from the front of the queue.
+    Visit all the neighbors of this node.
+    If any of the neighbors was already visited then it must be present in the visited dictionary. Get the clone of this neighbor from visited in that case.
+    Otherwise, create a clone and store in the visited.
+    Add the clones of the neighbors to the corresponding list of the clone node.
+    """
+    
+"""
 Time Complexity : O(N + M), where NN is a number of nodes (vertices) and MM is a number of edges.
 
 Space Complexity : O(N). 
-This space is occupied by the visited dictionary and in addition to that, space would also be occupied by the queue since we are adopting the BFS approach here. 
-The space occupied by the queue would be equal to O(W) where WW is the width of the graph. Overall, the space complexity would be O(N).
+This space is occupied by the visited dictionary and in addition to that, space would also be occupied by 
+the queue since we are adopting the BFS approach here. 
+
+The space occupied by the queue would be equal to O(W) where WW is the width of the graph. 
+Overall, the space complexity would be O(N).
 """
